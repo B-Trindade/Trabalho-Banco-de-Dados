b11ebf92f26cc69345e553a4553972dcccb5b4a1
B-Trindade <bttostes@gmail.com>
2019-11-17 22:35:44 -0300

    started setting up connection
    still no response in localhost:3000, as expected.

--- index.js
+++ index.js
@@ -1,6 +1,22 @@
 const express = require('express');
 const mysql = require('mysql');
 
+//Creates connection
+const connection = mysql.createConnection({
+    host        :       'localhost',
+    user        :       'root',
+    database    :       'trabalho'
+});
+
+//Connect
+connection.connect((err) => {
+    if(err){
+        console.log('Erro:' + err);
+    }
+    console.log('MySQL está conectado!');
+});
+
+
 const app = express();
 
 app.listen('3000', () => {
